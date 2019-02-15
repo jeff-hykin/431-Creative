c49a0aaa125b052c082d9b18903a72b490145a2f
Jeff Hykin <jeffhykin@jeffs-mbp-2.lan>
2019-02-15 16:42:11 -0600

    _color of button text problem

--- client/splash-page/splash-page.jsx
+++ client/splash-page/splash-page.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
-import { css } from '@emotion/core'
 import Button from '@material-ui/core/Button'
 
-export default ({ className }) => <div className={className}>
-  <h1>Howdy</h1>
-  Hello World
-  <Button
-    css={css`
-          background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
-          border-radius: 3px;
-          border: 0;
-          color: purple;
-          height: 48px;
-          padding: 0 30px;
-          box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
-        `}
-  >
-        Emotion
-  </Button>
-</div>
+export default class extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { colorOfButtonText: '' }
+  }
+
+  render () {
+    return <div className={this.props.className}>
+      <h1>Howdy</h1>
+      Hello World
+      <input placeholder='button color' onChange={e => this.setState({ colorOfButtonText: e.target.value })} />
+      <Button style={{ color: this.state.colorOfButtonText }} >
+            Emotion
+      </Button>
+    </div>
+  }
+}
