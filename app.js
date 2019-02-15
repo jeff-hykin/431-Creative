2c4f84b0f564dbc1b877f27f623b9359cd31d718
Thomas McDonald <thomasmcdonald@Thomass-MacBook-Pro.local>
2019-02-15 02:00:25 -0600

    working prod and development modes

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const { settings } = require('./package.json')
-const Bundler = require('parcel-bundler')
-const app = require('express')()
+const express = require('express')
+const app = express()
 
-const file = 'client/index.html' // Pass an absolute path to the entrypoint here
-const options = {} // See options section of api docs, for the possibilities
+if (process.env.NODE_ENV === 'development') {
+  const Bundler = require('parcel-bundler')
+  const errorhandler = require('errorhandler')
+  // Error handling depending on dev/prod mode
+  app.use(errorhandler())
 
-// Initialize a new bundler using a file and options
-const bundler = new Bundler(file, options)
+  const file = 'client/index.html' // Pass an absolute path to the entrypoint here
+  const options = {} // See options section of api docs, for the possibilities
 
-// Let express use the bundler middleware, this will let Parcel handle every request over your express server
-app.use(bundler.middleware())
+  // Initialize a new bundler using a file and options
+  const bundler = new Bundler(file, options)
+
+  // Let express use the bundler middleware, this will let Parcel handle every request over your express server
+  app.use(bundler.middleware())
+} else if (process.env.NODE_ENV === 'production') {
+  const path = require('path')
+  // use bundled static files
+  app.use(express.static('dist'))
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve('./dist/index.html'))
+  })
+}
 
 // Listen on port 8080
-app.listen(settings.PORT, () => { console.log(`\nApp is running on http://localhost:${settings.PORT}`) })
+app.listen(settings.PORT, () => { console.log(`\nApp is running on http://localhost:${settings.PORT} in ${process.env.NODE_ENV} mode`) })
