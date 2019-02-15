b41d374f65615e6635c2463e59a687853cda1673
Grace Westerman <gracewes@tamu.edu>
2019-02-14 18:17:14 -0600

    mocha dummy test set up, npm test commmand runs mocha

--- tests/dummyTest.js
+++
@@ -1,5 +0,0 @@
-let dummyFunction = require('../backend/functions/dummyFunction')
-let output = dummyFunction()
-if (output !== 'This is a dummy function') {
-  throw Error('dummyFunction failed test')
-}
