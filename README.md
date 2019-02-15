34a00d53cb996bbac0405b6f2b211d904c422afc
Joseph Martinsen <joseph@martinsen.com>
2019-02-14 19:05:23 -0600

    Add React and npm run command to bring up client A dev can now bring up a client running on localhost:3000 with a react component mounted

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 [![Coverage Status](https://coveralls.io/repos/github/jeff-hykin/431-Creative/badge.svg?branch=master)](https://coveralls.io/github/jeff-hykin/431-Creative?branch=master)
+
 # 431-Creative
+
+## Client
+
+```bash
+npm run client
+```
---
+++ client/main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Howdy</h1>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
