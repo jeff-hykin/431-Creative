a43d9a8244fc1b1da28110cd82992e3b7e607315
Thomas McDonald <thomasmcdonald@Thomass-MacBook-Pro.local>
2019-02-15 02:22:27 -0600

    Added css baseline to app for material-ui

--- client/main.jsx
+++ client/main.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import SplashPage from './splashPage/index'
 import NoMatch from './nomatch/index'
 
 function App () {
   return (
-    <div className='App'>
-      <h1>Howdy</h1>
-      <Switch>
-        <Route exact path='/' component={SplashPage} />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <div className='App'>
+        <h1>Howdy</h1>
+        <Switch>
+          <Route exact path='/' component={SplashPage} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </React.Fragment>
   )
 }
 
