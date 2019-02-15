779ebce5d39b3d4a6ae4d51f8381cdc657afb3ad
Jeff Hykin <jeffhykin@jeffs-mbp-2.lan>
2019-02-15 16:06:46 -0600

    fix flash of unstyled content

--- client/main.jsx
+++ client/main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Transitioner from './transitioner'
-import { HEAD, BODY, TITLE, DIV, LINK, NOSCRIPT } from 'good-dom'
+import { TITLE, DIV, NOSCRIPT } from 'good-dom'
 // pages
 import SplashPage from './splash-page/splash-page'
 import PageNotFound from './page-not-found/page-not-found'
@@ -12,22 +12,18 @@ import { withStyles } from '@material-ui/core'
 //
 // set document head
 //
-document.head = new HEAD(
-  new TITLE('ACC'),
-  // stylesheets
-  new LINK({ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500' }),
-  new LINK({ rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }),
-  new LINK({ rel: 'stylesheet', href: 'https://unpkg.com/css-baseline/css/super-1.css' })
+document.head.add(
+  new TITLE('ACC')
 )
 
 //
 // set body
 //
 let reactContainer = new DIV()
-document.body = new BODY(
+document.body.children = [
   new NOSCRIPT('You need to enable JavaScript to run this app.'),
   reactContainer
-)
+]
 
 //
 // Page styles
@@ -66,7 +62,8 @@ let App = withStyles(classes)(props =>
     {/* Pick which page to render */}
     <Switch>
       <Page {...props} component={SplashPage} path='/' />
-      <Page {...props} component={PageNotFound} />
+      {/* <Page {...props} component={PageNotFound} /> */}
+      <Route path='/*' component={PageNotFound} />
     </Switch>
   </BrowserRouter>
 )
