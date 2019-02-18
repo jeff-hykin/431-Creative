11b511360dc81524dd22bb5d4f5ff14b0bddfbe9
Nicholas Enochs <enochnic@tamu.edu>
2019-02-18 00:12:04 -0600

    added split color for splash page but needs drop shadow still

--- client/splash-page/splash-page.jsx
+++ client/splash-page/splash-page.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
+import { withStyles } from '@material-ui/core'
 
-export default class extends React.Component {
+export const classes = {
+  page: {
+    flexDirection: 'column',
+    transition: 'color 1500ms ease-in-out'
+  },
+  pageNotLoaded: {
+    color: 'white'
+  },
+  pageFullyLoaded: {
+    color: 'black'
+  },
+  triangleMaroon: {
+    position: 'fixed',
+  	bottom: '0',
+  	right: '0',
+    borderLeft: '50vw solid transparent',
+    borderRight: '50vw solid #500000',
+    borderBottom: '50vh solid transparent',
+    borderTop: '50vh solid #500000',
+    width: '0',
+    height: '0'
+  },
+  triangleWhite: {
+    position: 'fixed',
+  	bottom: '0',
+  	right: '0',
+    borderLeft: '50vw solid #FFFFFF',
+    borderRight: '50vw solid transparent',
+    borderBottom: '50vh solid #FFFFFF',
+    borderTop: '50vh solid transparent',
+    width: '0',
+    height: '0'
+  }
+}
+export default withStyles(classes)(class extends React.Component {
   constructor (props) {
     super(props)
     this.state = { colorOfButtonText: '' }
@@ -9,12 +44,8 @@ export default class extends React.Component {
 
   render () {
     return <div className={this.props.className}>
-      <h1>Howdy</h1>
-      Hello World
-      <input placeholder='button text color' onChange={e => this.setState({ colorOfButtonText: e.target.value })} />
-      <Button style={{ color: this.state.colorOfButtonText }} >
-        Emotion
-      </Button>
+      <div className={this.props.classes.triangleMaroon} />
+      <div className={this.props.classes.triangleWhite} />
     </div>
   }
-}
+})
