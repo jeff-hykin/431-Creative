fc610967b65fc33aa4ef09c208b0512f5d48f736
jeff-hykin <jeff.hykin@gmail.com>
2019-02-19 16:12:48 -0600

    _fix the styling problem

--- client/splash-page/splash-page.jsx
+++ client/splash-page/splash-page.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
+import { withStyles } from '@material-ui/core/styles'
 
-export default class extends React.Component {
+export const classes = {
+  'red-text': {
+    unset: 'all',
+    color: 'red'
+  }
+}
+
+export default withStyles(classes)(class extends React.Component {
   constructor (props) {
     super(props)
     this.state = { colorOfButtonText: '' }
@@ -13,8 +21,8 @@ export default class extends React.Component {
       Hello World
       <input placeholder='button text color' onChange={e => this.setState({ colorOfButtonText: e.target.value })} />
       <Button style={{ color: this.state.colorOfButtonText }} >
-        Emotion
+        <span className={this.props.classes['red-text']} >Test</span>
       </Button>
     </div>
   }
-}
+})
