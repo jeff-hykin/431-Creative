bad6d62976bcec80ee2f45be3b4e2e00cfcd927a
Nicholas Enochs <enochnic@tamu.edu>
2019-02-18 21:36:05 -0600

    added tool ar in need of themeing

--- client/splash-page/splash-page.jsx
+++ client/splash-page/splash-page.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 import { withStyles } from '@material-ui/core'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Grid from '@material-ui/core/Grid'
 
 export const classes = {
   page: {
@@ -15,8 +18,8 @@ export const classes = {
   },
   triangleMaroon: {
     position: 'fixed',
-  	bottom: '0',
-  	right: '0',
+    bottom: '0',
+    right: '0',
     borderLeft: '50vw solid transparent',
     borderRight: '50vw solid #500000',
     borderBottom: '50vh solid transparent',
@@ -26,14 +29,29 @@ export const classes = {
   },
   triangleWhite: {
     position: 'fixed',
-  	bottom: '0',
-  	right: '0',
+    bottom: '0',
+    right: '0',
     borderLeft: '50vw solid #FFFFFF',
     borderRight: '50vw solid transparent',
     borderBottom: '50vh solid #FFFFFF',
     borderTop: '50vh solid transparent',
     width: '0',
     height: '0'
+  },
+  mainDiv: {
+    position: 'fixed',
+    bottom: '0',
+    right: '0'
+  },
+  whiteButton: {
+    color: '#500000',
+    backgroundColor: '#FFFFFF',
+    '&:hover': {
+      backgroundColor: '#500000'
+    }
+  },
+  maxWidth: {
+    width: '100vw'
   }
 }
 export default withStyles(classes)(class extends React.Component {
@@ -44,8 +62,20 @@ export default withStyles(classes)(class extends React.Component {
 
   render () {
     return <div className={this.props.className}>
+      <AppBar className={this.props.classes.maxWidth} position='static'>
+        <Toolbar>
+          <Grid container direction='row' justify='flex-end' alignItems='center'>
+            <Button color='inherit'>Login</Button>
+          </Grid>
+        </Toolbar>
+      </AppBar>
       <div className={this.props.classes.triangleMaroon} />
       <div className={this.props.classes.triangleWhite} />
+      <div className={this.props.classes.mainDiv}>
+        <Button size='large' className={this.props.classes.whiteButton}>
+          Browse
+        </Button>
+      </div>
     </div>
   }
 })
