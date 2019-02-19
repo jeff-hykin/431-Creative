34cc8da9fe7b32785cce227d0207836f790ee507
Thomas McDonald <thomas.carnegie.1234@gmail.com>
2019-02-18 18:10:59 -0600

    moved the parcel dist folder to inside the static folder

--- scripts/prod.rb
+++ scripts/prod.rb
@@ -1,4 +1,5 @@
-puts `parcel build client/index.html`
+# puts `parcel build client/index.html -d static/dist`
+system "npm run heroku-postbuild"
 ENV["NODE_ENV"] = "production"
 system "node app.js"
 # --experimental-scope-hoisting
