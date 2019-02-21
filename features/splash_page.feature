01e64de9c9da063b28a19eec0aae7d279bf33aaf
mitcheld <mitcheld@tamu.edu>
2019-02-21 15:02:18 -0600

    initialize a splash page cucumber test(always fails currently)

---
+++ features/splash_page.feature
@@ -0,0 +1,7 @@
+Feature: Splash page functionality
+  People will want to click our splash page to do things
+
+  Scenario: I click on our splash page
+    Given I am on the splash page
+    When I try to click a button
+    Then something should happen
--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,2 +1,21 @@
-// const assert = require('assert')
-// const { Given, When, Then } = require('cucumber')
+const assert = require('assert')
+const { Given, When, Then } = require('cucumber')
+
+function testFunction (value) {
+  return value
+}
+
+Given('I am on the splash page', function () {
+  // Write code here that turns the phrase above into concrete actions
+  this.value = 'a value'
+})
+
+When('I try to click a button', function () {
+  // Write code here that turns the phrase above into concrete actions
+  this.actualResult = testFunction(this.value)
+})
+
+Then('something should happen', function (expectedResult) {
+  // Write code here that turns the phrase above into concrete actions
+  assert.strictEqual(this.actualResult, expectedResult)
+})
