be595777e239d9d574dd2cd9eab926b014ee05b0
Thomas McDonald <thomas.carnegie.1234@gmail.com>
2019-02-22 10:42:47 -0600

    make sure travis runs coveralls branch

--- scripts/coverage.rb
+++ scripts/coverage.rb
@@ -1,5 +1,9 @@
+cyan = "\x1b[36m"
+default = "\x1b[0m"
+
 ENV["NODE_ENV"] = "testing"
 if ENV["TRAVIS"] == true
+  puts cyan + 'Running tests and reporting coverage' + default
   system "nyc --all --reporter=lcov npm test && nyc report | coveralls"
 else
   system "nyc --all --reporter=lcov npm test && nyc report"
