a5c1afa0406e96dcea520855354990cbaffb6d4d
Nicholas Enochs <mkdirito@gmail.com>
2019-02-22 14:40:39 -0600

    forgot stepdefs

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,13 +1,10 @@
 import { configure, mount } from 'enzyme'
-// let shallow = require('enzyme')
+import Snackbar from '@material-ui/core/Snackbar'
 import React from 'react'
 import Adapter from 'enzyme-adapter-react-16'
-// let React = require('react')
 import SplashPage from '../../client/splash-page/splash-page'
-// let SplashPage = '../client/splash-page/splash-page'
 const assert = require('assert')
 const { Given, When, Then } = require('cucumber')
-
 const { JSDOM } = require('jsdom')
 const jsdom = new JSDOM('<!doctype html><html><body></body></html>')
 const { window } = jsdom
@@ -33,6 +30,5 @@ When('I click browse', function () {
 })
 
 Then('the state is changed', function () {
-  console.log(this.wrapper.state())
-  assert.strictEqual(this.wrapper.state('open'), 'true')
+  assert.strictEqual(this.wrapper.find(Snackbar).props('open'), 'true')
 })
