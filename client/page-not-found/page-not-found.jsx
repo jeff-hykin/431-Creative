8c0a6a8b3c33a419b89552e9c0483c1532ff44b7
jeff-hykin <jeff.hykin@gmail.com>
2019-02-22 15:32:03 -0600

    _remove circle cutout

--- client/page-not-found/page-not-found.jsx
+++ client/page-not-found/page-not-found.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import CircleCutout from '../global-components/circle-cutout'
 import MatrixParallax from './matrix-parallax'
 
 export default () =>
   <React.Fragment>
-    <CircleCutout />
     <MatrixParallax>
       <h2>404</h2>
       <h5 style={{ maxWidth: '100vw' }}>Page Not Found</h5>
