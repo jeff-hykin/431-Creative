70262cc4a6c7f9695d0fbdbc3704b884173aee10
Thomas McDonald <thomas.carnegie.1234@gmail.com>
2019-02-22 00:44:26 -0600

    database functions pass all tests

--- database/wrapper-impl.js
+++ database/wrapper-impl.js
@@ -3,6 +3,7 @@
 const mongo = require('mongodb')
 
 let db
+let dbo
 
 const DEFAULT_DB = 'devsearch'
 const POST_COLLECTION = 'posts'
@@ -13,13 +14,16 @@ async function connect (database = DEFAULT_DB) {
 
   if (process.env.NODE_ENV === 'development') {
     db = await mongo.MongoClient.connect(`mongodb://localhost:27017/${database}`, options)
+    dbo = db.db(database)
   } else if (process.env.NODE_ENV === 'testing') {
     db = await mongo.MongoClient.connect(`mongodb://localhost:27017/${database}-test`, options)
+    dbo = db.db(`${database}-test`)
   } else if (process.env.NODE_ENV === 'production') {
     db = await mongo.MongoClient.connect(`
     mongodb://dbAdmin:${process.env.DB_PASS}@prodcluster-shard-00-00-zwe3b.mongodb.net:27017,
     prodcluster-shard-00-01-zwe3b.mongodb.net:27017,
     prodcluster-shard-00-02-zwe3b.mongodb.net:27017/${database}?ssl=true&replicaSet=ProdCluster-shard-0&authSource=admin&retryWrites=true`, options)
+    dbo = db.db(database)
   }
   return db
 }
@@ -35,25 +39,25 @@ function getDb () {
 /* Create Operations */
 
 async function insertOnePost (document) {
-  let postCollection = db.collection(POST_COLLECTION)
+  let postCollection = dbo.collection(POST_COLLECTION)
   let result = await postCollection.insertOne(document)
   return result
 }
 
 async function insertManyPosts (documents, ordered = true) {
-  let postCollection = db.collection(POST_COLLECTION)
+  let postCollection = dbo.collection(POST_COLLECTION)
   let result = await postCollection.insertMany(documents, { ordered })
   return result
 }
 
 async function insertOneUser (document) {
-  let userCollection = db.collection(USER_COLLECTION)
+  let userCollection = dbo.collection(USER_COLLECTION)
   let result = await userCollection.insertOne(document)
   return result
 }
 
 async function insertManyUsers (documents, ordered = true) {
-  let userCollection = db.collection(USER_COLLECTION)
+  let userCollection = dbo.collection(USER_COLLECTION)
   let result = await userCollection.insertMany(documents, { ordered })
   return result
 }
@@ -61,25 +65,25 @@ async function insertManyUsers (documents, ordered = true) {
 /* Read Operations */
 
 async function findOnePost (query = {}, projection = {}) {
-  let postCollection = db.collection(POST_COLLECTION)
+  let postCollection = dbo.collection(POST_COLLECTION)
   let post = await postCollection.findOne(query, projection)
   return post
 }
 
 async function findManyPosts (query = {}, projection = {}) {
-  let postCollection = db.collection(POST_COLLECTION)
+  let postCollection = dbo.collection(POST_COLLECTION)
   let posts = await postCollection.find(query, projection).toArray()
   return posts
 }
 
 async function findOneUser (query, projection = {}) {
-  let userCollection = db.collection(USER_COLLECTION)
+  let userCollection = dbo.collection(USER_COLLECTION)
   let user = await userCollection.findOne(query, projection)
   return user
 }
 
 async function findManyUsers (query, projection = {}) {
-  let userCollection = db.collection(USER_COLLECTION)
+  let userCollection = dbo.collection(USER_COLLECTION)
   let users = await userCollection.find(query, projection).toArray()
   return users
 }
@@ -87,25 +91,25 @@ async function findManyUsers (query, projection = {}) {
 /* Update Operations */
 
 async function updateOnePost (filter, update, options = {}) {
-  let postCollection = db.collection(POST_COLLECTION)
+  let postCollection = dbo.collection(POST_COLLECTION)
   let resultDocument = await postCollection.updateOne(filter, update, options)
   return resultDocument
 }
 
 async function updateManyPosts (filter, update, options = {}) {
-  let postCollection = db.collection(POST_COLLECTION)
+  let postCollection = dbo.collection(POST_COLLECTION)
   let resultDocument = await postCollection.updateMany(filter, update, options)
   return resultDocument
 }
 
 async function updateOneUser (filter, update, options = {}) {
-  let userCollection = db.collection(USER_COLLECTION)
+  let userCollection = dbo.collection(USER_COLLECTION)
   let resultDocument = await userCollection.updateOne(filter, update, options)
   return resultDocument
 }
 
 async function updateManyUsers (filter, update, options = {}) {
-  let userCollection = db.collection(USER_COLLECTION)
+  let userCollection = dbo.collection(USER_COLLECTION)
   let resultDocument = await userCollection.updateMany(filter, update, options)
   return resultDocument
 }
@@ -113,25 +117,25 @@ async function updateManyUsers (filter, update, options = {}) {
 /* Delete Operations */
 
 async function deleteOnePost (filter, options = {}) {
-  let postCollection = db.collection(POST_COLLECTION)
+  let postCollection = dbo.collection(POST_COLLECTION)
   let resultDocument = await postCollection.deleteOne(filter, options)
   return resultDocument
 }
 
 async function deleteManyPosts (filter, options = {}) {
-  let postCollection = db.collection(POST_COLLECTION)
+  let postCollection = dbo.collection(POST_COLLECTION)
   let resultDocument = await postCollection.deleteMany(filter, options)
   return resultDocument
 }
 
 async function deleteOneUser (filter, options = {}) {
-  let userCollection = db.collection(USER_COLLECTION)
+  let userCollection = dbo.collection(USER_COLLECTION)
   let resultDocument = await userCollection.deleteOne(filter, options)
   return resultDocument
 }
 
 async function deleteManyUsers (filter, options = {}) {
-  let userCollection = db.collection(USER_COLLECTION)
+  let userCollection = dbo.collection(USER_COLLECTION)
   let resultDocument = await userCollection.deleteMany(filter, options)
   return resultDocument
 }
