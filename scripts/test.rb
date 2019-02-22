fe3dd47db2c6d0d06241e94551b1e15436d41eca
Nicholas Enochs <mkdirito@gmail.com>
2019-02-22 15:04:51 -0600

    removed example cucumbers and fixed test.rb

--- scripts/test.rb
+++ scripts/test.rb
@@ -1,3 +1,3 @@
 system "npm run database"
 ENV["NODE_ENV"] = "testing"
-system `standard && mocha && npx babel-node --presets=@babel/react,@babel/preset-env node_modules/cucumber/bin/cucumber-js`
+system "standard && mocha && npx babel-node --presets=@babel/react,@babel/preset-env node_modules/cucumber/bin/cucumber-js"
