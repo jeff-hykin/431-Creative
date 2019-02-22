fbc210fd8fec407ad67fe27d3418f1045406b8cc
Thomas McDonald <thomas.carnegie.1234@gmail.com>
2019-02-22 10:49:10 -0600

    Displaying TRAVIS env variable

--- scripts/coverage.rb
+++ scripts/coverage.rb
@@ -1,7 +1,9 @@
 cyan = "\x1b[36m"
+blue = "\x1b[34m"
 default = "\x1b[0m"
 
 ENV["NODE_ENV"] = "testing"
+puts blue + "TRAVIS: #{ENV["TRAVIS"]}" + default
 if ENV["TRAVIS"] == true
   puts cyan + 'Running tests and reporting coverage' + default
   system "nyc --all --reporter=lcov npm test && nyc report | coveralls"
