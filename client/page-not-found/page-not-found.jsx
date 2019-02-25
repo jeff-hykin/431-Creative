077d3f9218ba0e0de355177a4312d98e6da75468
jeff-hykin <jeff.hykin@gmail.com>
2019-02-25 17:33:20 -0600

    add colors

--- client/page-not-found/page-not-found.jsx
+++ client/page-not-found/page-not-found.jsx
@@ -3,8 +3,8 @@ import MatrixParallax from 'react-matrix-parallax'
 
 export default () =>
   <div id='page-not-found' >
-    <MatrixParallax>
+    <MatrixParallax color='black' backgroundColor='white' >
       <h2>404</h2>
-      <h5 style={{ maxWidth: '100vw' }}>Page Not Found</h5>
+      <h5 style={{ maxWidth: '100vw', fontFamily: 'Consolas,monospace' }}>Page Not Found</h5>
     </MatrixParallax>
   </div>
