609c7c6c786dc8c54f70b81839deb0f304ae57d6
jeff-hykin <jeff.hykin@gmail.com>
2019-02-25 13:18:01 -0600

    fix test to make it start the server

--- scripts/test.rb
+++ scripts/test.rb
@@ -1,4 +1,5 @@
 system "npm run database"
+system "npm run start &"
 ENV["NODE_ENV"] = "testing"
 exit_success = system "standard && mocha && npx babel-node --presets=@babel/react,@babel/preset-env node_modules/cucumber/bin/cucumber-js"
 if not exit_success
