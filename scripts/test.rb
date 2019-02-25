bf2ee9495979fb89b2d237c4572642ff31b50d7c
jeff-hykin <jeff.hykin@gmail.com>
2019-02-25 17:21:12 -0600

    restore the original test.rb script, then att --exit to prevent hanging

--- scripts/test.rb
+++ scripts/test.rb
@@ -1,7 +1,9 @@
 # tell the code its running in testing mode
 ENV["NODE_ENV"] = "testing"
+# startup the database
+system "npm run database"
 # run standard, mocha, and then compile cucumber and run cucumber
-exit_success = system "standard && mocha && npx babel-node --presets=@babel/react,@babel/preset-env node_modules/cucumber/bin/cucumber-js"
+exit_success = system "standard && mocha && npx babel-node --presets=@babel/react,@babel/preset-env node_modules/cucumber/bin/cucumber-js --exit"
 # if cucumber failed, then this process failed (maunal check)
 if not exit_success
   exit 1
