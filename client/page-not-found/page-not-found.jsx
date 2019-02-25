ce201d759d0244c57a0eff707536a918519052a1
jeff-hykin <jeff.hykin@gmail.com>
2019-02-25 01:44:56 -0600

    extract out the matrix package

--- client/page-not-found/page-not-found.jsx
+++ client/page-not-found/page-not-found.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import MatrixParallax from './matrix-parallax'
+import MatrixParallax from 'react-matrix-parallax'
 
 export default () =>
   <React.Fragment>
-    <MatrixParallax>
+    <MatrixParallax color='black' backgroundColor='white' >
       <h2>404</h2>
-      <h5 style={{ maxWidth: '100vw' }}>Page Not Found</h5>
+      <h5 style={{ maxWidth: '100vw', fontFamily: 'Consolas,monospace' }}>Page Not Found</h5>
     </MatrixParallax>
   </React.Fragment>
