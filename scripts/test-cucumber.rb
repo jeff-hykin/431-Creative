82738e679f23bc6027e1cc95f4dfa213f4d3cbcd
jeff-hykin <jeff.hykin@gmail.com>
2019-02-25 18:01:59 -0600

    make cucumber test not hang

--- scripts/test-cucumber.rb
+++ scripts/test-cucumber.rb
@@ -1,7 +1,7 @@
 # tell the code its running in testing mode
 ENV["NODE_ENV"] = "testing"
 # run standard, mocha, and then compile cucumber and run cucumber
-exit_success = system "npx babel-node --presets=@babel/react,@babel/preset-env node_modules/cucumber/bin/cucumber-js"
+exit_success = system "npx babel-node --presets=@babel/react,@babel/preset-env node_modules/cucumber/bin/cucumber-js --exit"
 # if cucumber failed, then this process failed (maunal check)
 if not exit_success
   exit 1
