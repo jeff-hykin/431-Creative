f6dd8c62c7d5ebbc271b644723bd3a4e160cde89
Grace Westerman <gracewes@tamu.edu>
2019-02-27 13:34:42 -0600

    added skeleton for tests for attribute card

---
+++ test/client/shared-components/attribute-card.test.js
@@ -0,0 +1,31 @@
+// import { shallow } from 'enzyme'
+// import React from 'react'
+// import { expect } from 'chai'
+// import AttributeCard from '../../../client/shared-components/attribute-card'
+
+describe('<AttributeCard />', () => {
+  it('contains the title of the card', () => {
+    // Example: Skills
+  })
+  it('contains the description on the card', () => {
+    // Example: List of skills
+  })
+  describe('Editing mode', () => {
+    it('has an edit button', () => {
+      // a pencil icon in the mockups
+    })
+    it('has a delete button', () => {
+      // a red X in the mockups
+    })
+    it('allows you to change the text', () => {
+    })
+  })
+  describe('non editing mode', () => {
+    it('does not have an edit button', () => {
+    })
+    it('does not have a delete button', () => {
+    })
+    it('does not allow you to change the text', () => {
+    })
+  })
+})
