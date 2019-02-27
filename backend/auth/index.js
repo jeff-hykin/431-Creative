c83ac5aa53c680bd3e81ddfd53a44423f9d85244
Thomas McDonald <thomas.carnegie.1234@gmail.com>
2019-02-26 19:52:34 -0600

    some changes regarding configuration. more to come

--- backend/auth/index.js
+++ backend/auth/index.js
@@ -2,16 +2,19 @@ const passport = require('passport')
 let GoogleStrategy = require('passport-google-oauth20').Strategy
 const os = require('os')
 const { settings } = require('../../package.json')
+const { google } = require('../../secrets.json')
 
 const _db = require('../../database/wrapper')
 
 const PORT = process.env.PORT || settings.PORT
+const CLIENT_ID = process.env.CLIENT_ID || google.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET || google.CLIENT_SECRET
 
 /* Passport Config */
 passport.use(new GoogleStrategy({
-  clientID: process.env['GOOGLE_CLIENT_ID'],
-  clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-  callbackURL: `http://${os.hostname()}:${PORT}/auth/google/callback`
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  callbackURL: `http://${os.hostname()}:${PORT}/auth/google/callback` // FIXME: Make the hostname for prod be the heroku app name. Add it to package.json. Also remove port
 }, async (accessToken, refreshToken, email, cb) => {
   try {
       let user = await _db.db.collections.users.findOne({ email: email['emails'][0].value })
@@ -52,4 +55,8 @@ module.exports.setupGoogleAuth = (app) => {
       // On success
       res.redirect('/')
   })
+}
+
+module.exports = {
+  passport
 }
