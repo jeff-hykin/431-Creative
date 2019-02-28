1c2c31b2541739ad7dcf2ed469b567efb1948eb2
Grace Westerman <gracewes@tamu.edu>
2019-02-28 08:18:39 -0600

    added skeleton for tests to account for users who are logged in vs not logged in

--- test/client/shared-components/attribute-card.test.js
+++ test/client/shared-components/attribute-card.test.js
@@ -11,21 +11,32 @@ describe('<AttributeCard />', () => {
     // Example: List of skills
   })
   describe('Editing mode', () => {
-    it('has an edit button', () => {
-      // a pencil icon in the mockups
+    it('has a submit button', () => {
+      // to finalize the changes that were made to the card
     })
     it('has a delete button', () => {
-      // a red X in the mockups
+      // a red X in the mockups, removes the card and data
     })
     it('allows you to change the text', () => {
     })
   })
   describe('non editing mode', () => {
-    it('does not have an edit button', () => {
+    describe('logged in user who created the card', () => {
+      it('has edit button', () => {
+        // a pencil in the mockups, should change the state of the card to editing mode
+      })
+      it('does not have a delete button', () => {
+      })
+      it('does not allow you to change the text', () => {
+      })
     })
-    it('does not have a delete button', () => {
-    })
-    it('does not allow you to change the text', () => {
+    describe('non logged in/ non owner view', () => {
+      it('does not have an editing button', () => {
+      })
+      it('does not have a delete button', () => {
+      })
+      it('does not allow you to change the text', () => {
+      })
     })
   })
 })
