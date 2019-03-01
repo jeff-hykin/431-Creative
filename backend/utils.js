375a8aa7ff871376f0a723d8eb6f80f7185ab758
Thomas McDonald <thomas.carnegie.1234@gmail.com>
2019-02-28 19:17:38 -0600

    modified createUser function

--- backend/utils.js
+++ backend/utils.js
@@ -1,18 +1,18 @@
 const uuidv4 = require('uuid/v4')
 const _db = require('../database/wrapper')
 
-async function createUser (email, firstName = '', lastName = '') {
-  let currentTime = Date.now()
+async function createUser (email, firstName = '', lastName = '', role = '') {
+  let date = new Date()
   let user = {
     _id: uuidv4(),
     email,
     firstName,
     lastName,
-    dateCreated: currentTime,
-    dateModified: currentTime,
+    dateCreated: date,
+    dateModified: date,
     myPosts: [], // list of post Ids
-    fields: [] // list of objects representing fields
-    // TODO: add role
+    fields: [], // list of objects representing fields
+    role
   }
   await _db.db.collections.users.insertOne(user)
 }
