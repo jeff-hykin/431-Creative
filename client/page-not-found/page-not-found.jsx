a1bd9935aff3a7be05e2abd672eeb6b3ca06c2d2
Grace Westerman <gracewes@tamu.edu>
2019-03-03 16:38:56 -0600

    made the words at least show up on the page infront of the matrix

--- client/page-not-found/page-not-found.jsx
+++ client/page-not-found/page-not-found.jsx
@@ -3,8 +3,10 @@ import MatrixParallax from 'react-matrix-parallax'
 
 export default () =>
   <div id='page-not-found' >
+    <div style={{ position: 'relative', zIndex: 5, textAlign: 'center' }}>
+      <p><h2>404</h2></p>
+      <p><h5 style={{ maxWidth: '100vw', fontFamily: 'Consolas,monospace' }}>Page Not Found</h5></p>
+    </div>
     <MatrixParallax color='black' backgroundColor='white' >
-      <h2>404</h2>
-      <h5 style={{ maxWidth: '100vw', fontFamily: 'Consolas,monospace' }}>Page Not Found</h5>
     </MatrixParallax>
   </div>
