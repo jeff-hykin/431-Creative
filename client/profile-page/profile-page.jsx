200f2cc01c7e638827a92a2e643576393bccd445
mitcheld <mitcheld@tamu.edu>
2019-03-05 16:17:34 -0600

    try to add header bar

--- client/profile-page/profile-page.jsx
+++ client/profile-page/profile-page.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
+// import Button from '@material-ui/core/Button'
 import { withStyles } from '@material-ui/core'
 import 'react-toastify/dist/ReactToastify.min.css'
+import { colors } from '../theme'
 
-export const classes = {}
+export const classes = {
+  titleBar: {
+    paddingLeft: '3%',
+    paddingRight: '3%',
+    paddingTop: '1%',
+    paddingBottom: '1%',
+    backgroundColor: colors.teal,
+    width: '100%',
+    height: '20%'
+  }
+}
 
 export default withStyles(classes)(class extends React.Component {
   render () {
     return <div id='ProfilePage' className={this.props.className}>
-      <h5>
-        profile
-      </h5>
+      <div className={this.props.classes.titleBar}>
+        <h5>
+          Your Name
+        </h5>
+      </div>
     </div>
   }
 })
