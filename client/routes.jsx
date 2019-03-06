82c396336f5db06055bd8430f53642d7bf1b12c5
jeff-hykin <jeff.hykin@gmail.com>
2019-03-06 11:05:04 -0600

    make standard js happy

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,7 +1,6 @@
 import 'regenerator-runtime/runtime'
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Page from './page'
 
 // pages
 import SplashPage from './splash-page/splash-page'
