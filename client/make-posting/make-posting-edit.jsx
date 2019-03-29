76d67ec11363cbd7519e53abf6fd112f6f205b58
Thomas McDonald <thomas.carnegie.1234@gmail.com>
2019-03-29 14:04:23 -0500

    Completed new style

--- client/make-posting/make-posting-edit.jsx
+++ client/make-posting/make-posting-edit.jsx
@@ -8,10 +8,13 @@ import TextField from '@material-ui/core/TextField'
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
+import CardHeader from '@material-ui/core/CardHeader'
 import Chip from '@material-ui/core/Chip'
 import Navbar from '../components/navbar'
 import { api } from '../../backend/setup-functions'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import Typography from '@material-ui/core/Typography'
+import Snackbar from '@material-ui/core/Snackbar'
 
 let buttonStyles = {
   borderRadius: '10rem',
@@ -20,53 +23,6 @@ let buttonStyles = {
 }
 
 const classes = theme => ({
-  addSkill: {
-    ...buttonStyles,
-    color: colors.white,
-    borderColor: colors.white,
-    backgroundColor: colors.teal,
-    marginRight: '2%',
-    marginLeft: '2%'
-  },
-  content: {
-    width: '100%',
-    height: '78vh',
-    display: 'flex',
-    paddingTop: '1rem',
-    backgroundColor: colors.offWhite
-  },
-  contacts: {
-    width: '30%',
-    height: '100%',
-    paddingLeft: '3%',
-    paddingRight: '3%'
-  },
-  bigFont: {
-    fontSize: 'calc(2.2vw + 1rem)'
-  },
-  alignBottom: {
-    display: 'flex',
-    flexDirection: 'column-reverse',
-    padding: '1rem'
-  },
-  card: {
-    height: '98%',
-    width: '98%'
-  },
-  skills: {
-    height: '15%',
-    width: '100%',
-    paddingTop: '1%'
-  },
-  contactsCard: {
-    height: '99%'
-  },
-  titleContainer: {
-    flexBasis: '0',
-    flexGrow: '1',
-    justifyContent: 'center',
-    width: '80%'
-  },
   root: {
     backgroundColor: colors.offWhite,
     width: '100vw',
@@ -81,12 +37,36 @@ const classes = theme => ({
   },
   heroContent: {
     maxWidth: 600,
-    margin: '0 auto',
-    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`
+    margin: '0 auto 24px auto'
   },
   appBar: {
     position: 'relative',
     background: '#2096F3',
+    color: 'white'
+  },
+  inputFields: {
+    margin: '4px',
+    display: 'block'
+  },
+  contactHeader: {
+    marginTop: '12px'
+  },
+  skillInput: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  description: {
+    marginTop: '12px'
+  },
+  snackBarError: {
+    background: '#d32f2f'
+  },
+  saveButton: {
+    background: '#2fd32f',
+    color: 'white'
+  },
+  cancelButton: {
+    background: '#d32f2f',
     color: 'white'
   }
 })
@@ -113,6 +93,14 @@ class MakePosting extends Component {
     this.setState(temp)
   }
 
+  handleSkillSnackBarClose = () => {
+    this.setState({ skillSnackBarOpen: false })
+  }
+
+  handleErrorSnackBarClose = () => {
+    this.setState({ errorSnackBarOpen: false })
+  }
+
   /* istanbul ignore next */
   savePosting = (e) => {
     /* istanbul ignore next */
@@ -126,15 +114,26 @@ class MakePosting extends Component {
     /* istanbul ignore next */
     newPost.skills = this.state.skills
     /* istanbul ignore next */
-    api['make-posting'](newPost).then(response => { console.log(response) })
+    api['make-posting'](newPost).then(response => {
+      console.log(response)
+      this.props.history.push('/dashboard') 
+    }).catch(err => {
+      let msg
+      if(err.toString() === 'Error: missing post parameters') msg = 'Missing Form Fields'
+      if(err.toString() === 'Error: not authorized') msg = 'You need to be logged in'
+      this.setState({ errorSnackBarOpen: true, errorMsg: msg })
+      console.error(err)
+    })
   }
 
   addSkill = (e) => {
     let temp = this.state
-    if (temp.newSkill !== '') {
+    if (temp.newSkill !== '' && !(temp.skills.includes(temp.newSkill))) {
       temp.skills.push(temp.newSkill)
       temp.newSkill = ''
       this.setState(temp)
+    } else {
+      this.setState({ skillSnackBarOpen: true })
     }
   }
 
@@ -145,32 +144,43 @@ class MakePosting extends Component {
 
   constructor (props) {
     super(props)
-    this.state = { title: '', description: '', skills: [], contact: {}, newSkill: '' }
+    this.state = { title: '', description: '', skills: [], contact: {}, newSkill: '', skillSnackBarOpen: false, errorSnackBarOpen: false, errorMsg: '' }
   }
 
   render () {
+
     return <div id='makePosting' className={this.props.className} style={{ width: '100%' }}>
 
       <CssBaseline />
       <Navbar title='Make a Post' />
       <section className={this.props.classes.root}>
         <Card className={this.props.classes.heroContent}>
+          <CardHeader title='Make a Post' />
           <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Post Information
+            </Typography>
             <TextField
               id='title'
               placeholder='Title'
-              value={this.state.title} />
-            <div id='skills-add'>
-              <TextField
-                id='skill'
-                placeholder='Skills'
-                className={classes.textField}
-                value={this.state.newSkill}
-                onChange={this.handleChange('newSkill')}
-              />
-              <Button id='skillButton' variant='outlined' classesonClick={this.addSkill}>
-                Add Skill
-              </Button>
+              value={this.state.title}
+              label='Post Title' 
+              onChange={this.handleChange('title')}
+              className={this.props.classes.inputFields} />
+            <div>
+              <div className={this.props.classes.skillInput}>
+                <TextField
+                  id='skill'
+                  placeholder='Skills'
+                  className={this.props.classes.inputFields}
+                  value={this.state.newSkill}
+                  onChange={this.handleChange('newSkill')}
+                  label='Add a Skill'
+                />
+                <Button id='skillButton' variant='outlined' onClick={this.addSkill} className={this.props.inputFields}>
+                  Add Skill
+                </Button>
+              </div>
               <div>
                 { this.state.skills.map((item) => (
                   <Chip
@@ -189,18 +199,87 @@ class MakePosting extends Component {
               placeholder='description'
               rows='15'
               variant='outlined'
+              fullWidth
+              label='Job Description'
+            />
+            {/* CONTACT INFO */}
+            <Typography color="textSecondary" gutterBottom className={this.props.classes.contactHeader}>
+              Post Contact Information
+            </Typography>
+            <TextField
+              id='postEmail'
+              label='Email'
+              name='Email'
+              className={this.props.classes.inputFields}
+              value={this.state.email}
+              onChange={this.handleContactChange('email')}
+            />
+            <TextField
+              id='post'
+              label='Company'
+              name='Title'
+              className={this.props.classes.inputFields}
+              value={this.state.company}
+              onChange={this.handleContactChange('company')}
+            />
+            <TextField
+              id='postName'
+              label='Phone'
+              name='Title'
+              className={this.props.classes.inputFields}
+              value={this.state.phone}
+              onChange={this.handleContactChange('phone')}
+            />
+            <TextField
+              id='postName'
+              label='LinkedIn'
+              name='Title'
+              className={this.props.classes.inputFields}
+              value={this.state.linkedin}
+              onChange={this.handleContactChange('linkedin')}
             />
           </CardContent>
           <CardActions>
-            <Button id='cancelButton' variant='outlined' className={this.props.classes.cancelButton} onClick={this.goHome}>
+            <Button id='cancelButton' variant='outlined' onClick={this.goHome}
+              classes={{
+                root: this.props.classes.cancelButton
+              }}>
               Cancel
             </Button>
-            <Button id='saveButton' variant='outlined' className={this.props.classes.saveButton} onClick={this.savePosting}>
+            <Button id='saveButton' variant='outlined' onClick={this.savePosting}
+              classes={{
+                root: this.props.classes.saveButton
+              }}>
               Save
             </Button>
           </CardActions>
         </Card>
       </section>
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        open={this.state.skillSnackBarOpen}
+        onClose={this.handleSkillSnackBarClose}
+        ContentProps={{
+          'aria-describedby': 'message-id',
+        }}
+        message={<span id="message-id">Skill is empty or already exists</span>}
+        autoHideDuration={2000}
+      />
+
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        open={this.state.errorSnackBarOpen}
+        onClose={this.handleErrorSnackBarClose}
+        ContentProps={{
+          classes: {
+            root: this.props.classes.snackBarError
+          },
+          'aria-describedby': 'message-id',
+        }}
+        message={<span id="message-id">{this.state.errorMsg}</span>}
+        autoHideDuration={2000}
+      />  
+      
     </div>
   }
 }
