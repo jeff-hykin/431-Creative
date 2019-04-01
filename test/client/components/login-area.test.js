165de954a6d10a3f47098d754ce85db3058e1249
Jeff Hykin <jeff.hykin@gmail.com>
2019-04-01 12:48:16 -0500

    add user testing tools

--- test/client/components/login-area.test.js
+++ test/client/components/login-area.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { expect } from 'chai'
+import * as tools from '../../tools'
 import { LoginArea, classes } from '../../../client/components/login-area'
 import BigButton from '../../../client/components/big-button'
 
@@ -11,22 +12,17 @@ describe('<LoginArea />', () => {
   }
 
   it('login button when no user', () => {
-    global.window.user = undefined
+    tools.unsetUser()
     const wrapper = shallow(<LoginArea {...fakeProps} />)
     expect(wrapper.exists('#loginButton')).to.equal(true)
     wrapper.find(BigButton).at(0).simulate('click')
   })
 
   it('logout button when user exists', () => {
-    global.user = {
-      username: 'bob'
-    }
-    global.window.user = global.user
+    tools.setNormalUser()
     const wrapper = shallow(<LoginArea {...fakeProps} />)
     expect(wrapper.exists('#logoutButton')).to.equal(true)
     wrapper.find('#logoutButton').simulate('click')
-    console.log(`history is:`, fakeProps.history)
-    global.user = undefined
-    global.window.user = undefined
+    tools.unsetUser()
   })
 })
