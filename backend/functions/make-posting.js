9773965bb8cbd8cafdfc34baa154959ee38ba778
Thomas McDonald <thomas.carnegie.1234@gmail.com>
2019-04-04 13:18:27 -0500

    Wip

--- backend/functions/make-posting.js
+++ backend/functions/make-posting.js
@@ -3,6 +3,6 @@ const { createPost } = require('../utils')
 module.exports = async (user, data, postId) => {
   if (!user) throw Error('not authorized')
   if (!data.title || !data.description || !data.contactInfo || !data.skills) throw Error('missing post parameters')
-
+  // TODO: Check to see if owner of post
   return createPost(user._id, data.title, data.description, data.contactInfo, data.skills, postId)
 }
