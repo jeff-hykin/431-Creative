37c1f65b55d42295a73703e16894ebe5f52755f1
Jeff Hykin <jeff.hykin@gmail.com>
2019-04-15 15:39:00 -0500

    add basic theme classes

--- client/theme.jsx
+++ client/theme.jsx
@@ -1,11 +1,3 @@
-export let classes = {}
-classes.vbox = {
-  display: 'flex',
-  alignItems: 'center',
-  alignContent: 'center',
-  flexDirection: 'column'
-}
-
 export const colors = {
   teal: '#00BFA5',
   red: '#ff6868',
@@ -14,12 +6,36 @@ export const colors = {
   offWhite: '#f5f3f3',
   white: '#FFFFFF',
   gray: '#80808087',
+  black: 'black',
   darkGray: 'gray',
   lightGray: '#e0e0e0'
 }
 
-classes.body = {
-  ...classes.vbox,
+export let style = {}
+
+style.vbox = {
+  display: 'flex',
+  alignItems: 'center',
+  alignContent: 'center',
+  flexDirection: 'column'
+}
+
+style.shadow = {
+  filter: 'drop-shadow(0px 10px 5px rgba(0,0,0,0.4))'
+}
+
+style.paper = {
+  filter: 'drop-shadow(0px 10px 5px rgba(0,0,0,0.4))',
+  backgroundColor: colors.white,
+  padding: '2rem 2.5rem',
+  margin: '1rem',
+  textAlign: 'center',
+  borderRadius: '0.4rem',
+  ...style.vbox
+}
+
+style.body = {
+  ...style.vbox,
   width: '100vw',
   minHeight: '100vh',
   overflow: 'visible',
