046d1b5f98f780fec0e39b94d952fe855898e155
Jeff Hykin <jeff.hykin@gmail.com>
2019-04-15 22:01:05 -0500

    _fix splash page test

--- test/client/splash-page/splash-page.test.js
+++ test/client/splash-page/splash-page.test.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { expect } from 'chai'
 import SplashPage from '../../../client/splash-page/splash-page'
 import Button from '@material-ui/core/Button'
+import BigButton from '../../../client/components/big-button'
 
 describe('<SplashPage />', () => {
   it('has login, browse, and create buttons', () => {
@@ -13,7 +14,8 @@ describe('<SplashPage />', () => {
         <SplashPage />
       </Router>
     )
-    expect(wrapper.find(Button)).to.have.lengthOf(3)
+    expect(wrapper.find(Button)).to.have.lengthOf(1)
+    expect(wrapper.find(BigButton)).to.have.lengthOf(3)
     for (let i = 0; i < wrapper.find(Button).length; i++) {
       wrapper.find(Button).at(i).simulate('click')
     }
