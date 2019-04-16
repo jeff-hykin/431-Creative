a20a2bd3f8f168d51747b3221919c927c290d127
Jeff Hykin <jeff.hykin@gmail.com>
2019-04-15 21:46:35 -0500

    fix paths (replace EditPosting/MakePosting) with PostingPage

--- test/client/components/posting-page.test.js
+++ test/client/components/posting-page.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { expect } from 'chai'
 import sinon from 'sinon'
-import { PostingPage, classes } from '../../../client/components/posting-page'
+import { PostingPage, classes } from '../../../client/posting-page/posting-page'
 
 describe('<PostingPage />', () => {
   let fakeProps = {
